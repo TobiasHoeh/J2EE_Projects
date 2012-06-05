bbe8e7efd4527edfd052f5b673ad433a11356518
Tobias Hoehmann <tobiashoehmann@googlemail.com>
2012-06-05 14:32:29 +0200

    Added spring-mvc-test project

---
+++ spring-mvc-test/src/main/java/de/tobias/common/controller/MovieController.java
@@ -0,0 +1,16 @@
+package de.tobias.common.controller;
+
+
+//@Controller
+//@RequestMapping("/movie")
+public class MovieController {
+
+	// @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	// public String getMovie(@PathVariable String name, ModelMap model) {
+	//
+	// model.addAttribute("movie", name);
+	// return "list";
+	//
+	// }
+
+}
---
+++ spring-mvc-test/src/main/webapp/WEB-INF/pages/compareResult.jsp
@@ -0,0 +1,9 @@
+<%@ page session="false" %>
+<html>
+    <head>
+        <title>Result</title>
+    </head>
+    <body>
+        <h1><c:out value="${output}"></c:out></h1>
+    </body>
+</html>
