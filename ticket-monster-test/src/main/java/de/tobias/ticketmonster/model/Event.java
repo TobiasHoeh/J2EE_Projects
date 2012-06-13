6a6817e4b0224714b82858fb8ebb3a79a04fca7a
Tobias Hoehmann <tobiashoehmann@googlemail.com>
2012-06-13 14:16:04 +0200

    Added entities to ticket-monster-app project

--- ticket-monster-test/src/main/java/de/tobias/ticketmonster/model/Event.java
+++ ticket-monster-test/src/main/java/de/tobias/ticketmonster/model/Event.java
@@ -1,124 +1,108 @@
 package de.tobias.ticketmonster.model;
 
+import static javax.persistence.GenerationType.IDENTITY;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.Version;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+/**
+ * <p>
+ * Represents an event, which may have multiple performances with different
+ * dates and venues.
+ * </p>
+ * 
+ * <p>
+ * Event's principle members are it's relationship to {@link EventCategory} -
+ * specifying the type of event it is - and {@link MediaItem} - providing the
+ * ability to add media (such as a picture) to the event for display. It also
+ * contains meta-data about the event, such as it's name and a description.
+ * </p>
+ * 
+ */
 @Entity
-public class Event implements java.io.Serializable {
+public class Event {
 
+	/* Declaration of fields */
+
+	/**
+	 * The synthetic ID of the object.
+	 */
 	@Id
-	private @GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "id", updatable = false, nullable = false)
-	Long id = null;
-	@Version
-	private @Column(name = "version")
-	int version = 0;
+	@GeneratedValue(strategy = IDENTITY)
+	private Long id;
 
-	public Long getId() {
-		return this.id;
-	}
-
-	public void setId(final Long id) {
-		this.id = id;
-	}
-
-	public int getVersion() {
-		return this.version;
-	}
-
-	public void setVersion(final int version) {
-		this.version = version;
-	}
-
-	@Override
-	public boolean equals(Object that) {
-		if (this == that) {
-			return true;
-		}
-		if (that == null) {
-			return false;
-		}
-		if (getClass() != that.getClass()) {
-			return false;
-		}
-		if (id != null) {
-			return id.equals(((Event) that).id);
-		}
-		return super.equals(that);
-	}
-
-	@Override
-	public int hashCode() {
-		if (id != null) {
-			return id.hashCode();
-		}
-		return super.hashCode();
-	}
-
+	/**
+	 * <p>
+	 * The name of the event.
+	 * </p>
+	 * 
+	 * <p>
+	 * The name of the event forms it's natural identity and cannot be shared
+	 * between events.
+	 * </p>
+	 * 
+	 * <p>
+	 * Two constraints are applied using Bean Validation
+	 * </p>
+	 * 
+	 * <ol>
+	 * <li><code>@NotNull</code> &mdash; the name must not be null.</li>
+	 * <li><code>@Size</code> &mdash; the name must be at least 5 characters and
+	 * no more than 50 characters. This allows for better formatting consistency
+	 * in the view layer.</li>
+	 * </ol>
+	 */
+	@Column(unique = true)
 	@NotNull
-	@Column
-	private @Size(message = "Must be > 5 and < 50", min = 5, max = 50)
-	String name;
+	@Size(min = 5, max = 50, message = "An event's name must contain between 5 and 50 characters")
+	private String name;
 
-	public String getName() {
-		return this.name;
-	}
+	/**
+	 * <p>
+	 * A description of the event.
+	 * </p>
+	 * 
+	 * <p>
+	 * Two constraints are applied using Bean Validation
+	 * </p>
+	 * 
+	 * <ol>
+	 * <li><code>@NotNull</code> &mdash; the description must not be null.</li>
+	 * <li><code>@Size</code> &mdash; the name must be at least 20 characters
+	 * and no more than 1000 characters. This allows for better formatting
+	 * consistency in the view layer, and also ensures that event organisers
+	 * provide at least some description - a classic example of a business
+	 * constraint.</li>
+	 * </ol>
+	 */
+	@NotNull
+	@Size(min = 20, max = 1000, message = "An event's name must contain between 20 and 1000 characters")
+	private String description;
 
-	public void setName(final String name) {
-		this.name = name;
-	}
-
-	@Column
-	private @Size(message = "Must be > 20 and < 1000", min = 20, max = 1000)
-	String description;
-
-	public String getDescription() {
-		return this.description;
-	}
-
-	public void setDescription(final String description) {
-		this.description = description;
-	}
-
-	@Column
-	private boolean major;
-
-	public boolean getMajor() {
-		return this.major;
-	}
-
-	public void setMajor(final boolean major) {
-		this.major = major;
-	}
-
-	@Column
-	private String picture;
-
-	public String getPicture() {
-		return this.picture;
-	}
-
-	public void setPicture(final String picture) {
-		this.picture = picture;
-	}
-
+	/**
+	 * <p>
+	 * A media item, such as an image, which can be used to entice a browser to
+	 * book a ticket.
+	 * </p>
+	 * 
+	 * <p>
+	 * Media items can be shared between events, so this is modeled as a
+	 * <code>@ManyToOne</code> relationship.
+	 * </p>
+	 * 
+	 * <p>
+	 * Adding a media item is optional, and the view layer will adapt if none is
+	 * provided.
+	 * </p>
+	 * 
+	 */
 	@ManyToOne
 	private MediaItem mediaItem;
-
-	public MediaItem getMediaItem() {
-		return mediaItem;
-	}
-
-	public void setMediaItem(MediaItem mediaItem) {
-		this.mediaItem = mediaItem;
-	}
 
 	/**
 	 * <p>
@@ -138,6 +122,32 @@ public class Event implements java.io.Serializable {
 	@NotNull
 	private EventCategory category;
 
+	/* Boilerplate getters and setters */
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public MediaItem getMediaItem() {
+		return mediaItem;
+	}
+
+	public void setMediaItem(MediaItem picture) {
+		this.mediaItem = picture;
+	}
+
 	public EventCategory getCategory() {
 		return category;
 	}
@@ -146,15 +156,41 @@ public class Event implements java.io.Serializable {
 		this.category = category;
 	}
 
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/*
+	 * toString(), equals() and hashCode() for Event, using the natural identity
+	 * of the object
+	 */
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+
+		Event event = (Event) o;
+
+		if (name != null ? !name.equals(event.name) : event.name != null)
+			return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		return name != null ? name.hashCode() : 0;
+	}
+
+	@Override
 	public String toString() {
-		String result = "";
-		if (name != null && !name.trim().isEmpty())
-			result += name;
-		if (description != null && !description.trim().isEmpty())
-			result += " " + description;
-		result += " " + major;
-		if (picture != null && !picture.trim().isEmpty())
-			result += " " + picture;
-		return result;
+		return name;
 	}
 }
--- ticket-monster-test/src/main/java/de/tobias/ticketmonster/model/Venue.java
+++ ticket-monster-test/src/main/java/de/tobias/ticketmonster/model/Venue.java
@@ -1,21 +1,101 @@
 package de.tobias.ticketmonster.model;
 
+import static javax.persistence.CascadeType.ALL;
+import static javax.persistence.FetchType.EAGER;
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ * <p>
+ * Represents a single venue
+ * </p>
+ * 
+ */
 @Entity
 public class Venue {
+
+	/* Declaration of fields */
+
+	/**
+	 * The synthetic id of the object.
+	 */
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = IDENTITY)
 	private Long id;
-	@NotNull
+
+	/**
+	 * <p>
+	 * The name of the event.
+	 * </p>
+	 * 
+	 * <p>
+	 * The name of the event forms it's natural identity and cannot be shared
+	 * between events.
+	 * </p>
+	 * 
+	 * <p>
+	 * The name must not be null and must be one or more characters, the Bean
+	 * Validation constraint <code>@NotEmpty</code> enforces this.
+	 * </p>
+	 */
+	@Column(unique = true)
+	@NotEmpty
 	private String name;
-	@NotNull
+
+	/**
+	 * The address of the venue
+	 */
+	@Embedded
+	private Address address = new Address();
+
+	/**
+	 * A description of the venue
+	 */
 	private String description;
-	@NotNull
+
+	/**
+	 * <p>
+	 * A set of sections in the venue
+	 * </p>
+	 * 
+	 * <p>
+	 * The <code>@OneToMany<code> JPA mapping establishes this relationship.
+	 * Collection members are fetched eagerly, so that they can be accessed even after the
+	 * entity has become detached. This relationship is bi-directional (a section knows which
+	 * venue it is part of), and the <code>mappedBy</code> attribute establishes
+	 * this. We cascade all persistence operations to the set of performances,
+	 * so, for example if a venue is removed, then all of it's sections will
+	 * also be removed.
+	 * </p>
+	 */
+	@OneToMany(cascade = ALL, fetch = EAGER, mappedBy = "venue")
+	private Set<Section> sections = new HashSet<Section>();
+
+	/**
+	 * The capacity of the venue
+	 */
 	private int capacity;
+
+	/**
+	 * An optional media item to entice punters to the venue. The
+	 * <code>@ManyToOne</code> establishes the relationship.
+	 */
+	@ManyToOne
+	private MediaItem mediaItem;
+
+	/* Boilerplate getters and setters */
 
 	public Long getId() {
 		return id;
@@ -33,6 +113,22 @@ public class Venue {
 		this.name = name;
 	}
 
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public MediaItem getMediaItem() {
+		return mediaItem;
+	}
+
+	public void setMediaItem(MediaItem description) {
+		this.mediaItem = description;
+	}
+
 	public String getDescription() {
 		return description;
 	}
@@ -41,11 +137,53 @@ public class Venue {
 		this.description = description;
 	}
 
+	public Set<Section> getSections() {
+		return sections;
+	}
+
+	public void setSections(Set<Section> sections) {
+		this.sections = sections;
+	}
+
 	public int getCapacity() {
 		return capacity;
 	}
 
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
+	}
+
+	/*
+	 * toString(), equals() and hashCode() for Venue, using the natural identity
+	 * of the object
+	 */
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+
+		Venue venue = (Venue) o;
+
+		if (address != null ? !address.equals(venue.address) : venue.address != null)
+			return false;
+		if (name != null ? !name.equals(venue.name) : venue.name != null)
+			return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = name != null ? name.hashCode() : 0;
+		result = 31 * result + (address != null ? address.hashCode() : 0);
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return name;
 	}
 }
